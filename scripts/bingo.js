88db960c6b239013454d9492c33b313fdafd5086
ArthurCallant <arthur.callant@hotmail.com>
2023-08-30 11:39:27 +0200

    🐛 Remove mention in diceRoll and add try catch wrapper

--- scripts/bingo.js
+++ scripts/bingo.js
@@ -1,9 +1,14 @@
+import { allCatcher } from './errors/handling.js';
 import { rollDice } from './utils/utils.js';
 
-export function getDiceRoll(msg, user) {
-  const diceRoll = rollDice();
-  const author = `<@${msg.author.id}>`;
+export function getDiceRoll(msg) {
+  try {
+    const diceRoll = rollDice();
+    const author = `${msg.author.username}`;
 
-  const message = `${author} rolled the dice and got a...\n\n${diceRoll}`;
-  msg.channel.send(message);
+    const message = `${author} rolled the dice and got a...\n\n${diceRoll}`;
+    msg.channel.send(message);
+  } catch (e) {
+    allCatcher(e, msg);
+  }
 }
